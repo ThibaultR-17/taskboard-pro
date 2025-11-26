568654a64c04a2374291c1ce2eabe61deb0d4c1d
Thibault <thibault.roux17@gmail.com>
2025-11-26 16:08:50 +0100

    feat : Lazy Loading complete

---
+++ taskboard-pro/src/app/features/tasks/tasks-page/tasks-page.ts
@@ -0,0 +1,46 @@
+import { Component,inject } from '@angular/core';
+import { TaskService } from '../../../core/service/task';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-tasks-page',
+  imports: [AsyncPipe],
+  templateUrl: './tasks-page.html',
+  styleUrl: './tasks-page.css',
+})
+export class TasksPage {
+
+    protected count=0;
+  private myIntervalles=0;
+
+
+  ngOnInit() {
+    this.myIntervalles= setInterval(()=>{
+      if (document.hasFocus()){
+        this.count++;
+        console.log(this.count);
+      }
+    },500
+    );
+  
+  }
+
+  tasks$!: ReturnType<TaskService['getTask']>;
+
+  constructor(private taskService:TaskService){
+    this.tasks$ = this.taskService.getTask();
+  }
+
+  taskService2 = inject(TaskService);
+  tasks2$=this.taskService2.tasks$;
+
+  addTask(title:string){
+    this.taskService2.addTask(title);
+  }
+
+
+  ngOnDestroy(){
+    console.log("onDestroy")
+    clearInterval(this.myIntervalles);
+  }
+}
--- taskboard-pro/src/app/home/home.ts
+++ taskboard-pro/src/app/home/home.ts
@@ -1,47 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { TaskService } from '../core/service/task';
+import { Component } from '@angular/core';
+import { TasksPage } from '../features/tasks/tasks-page/tasks-page';
 
 
 @Component({
   selector: 'app-home',
-  imports: [AsyncPipe],
+  imports: [TasksPage],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
 
-  protected count=0;
-  private myIntervalles=0;
-
-
-  ngOnInit() {
-    this.myIntervalles= setInterval(()=>{
-      if (document.hasFocus()){
-        this.count++;
-        console.log(this.count);
-      }
-    },500
-    );
-  
-  }
-
-  tasks$!: ReturnType<TaskService['getTask']>;
-
-  constructor(private taskService:TaskService){
-    this.tasks$ = this.taskService.getTask();
-  }
-
-  taskService2 = inject(TaskService);
-  tasks2$=this.taskService2.tasks$;
-
-  addTask(title:string){
-    this.taskService2.addTask(title);
-  }
-
-
-  ngOnDestroy(){
-    console.log("onDestroy")
-    clearInterval(this.myIntervalles);
-  }
 }
