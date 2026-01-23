2a2211b8e4566db044e03d9da3e5302a1ecf3ae3
Thibault <thibault.roux17@gmail.com>
2026-01-23 09:47:49 +0100

    Auto-generated commit

--- taskboard-pro/src/app/features/about/routes.ts
+++
@@ -1,6 +0,0 @@
-import {Routes} from '@angular/router'
-import { About } from './about'
-
-export const ABOUT_ROUTES : Routes = [
-    {path: '', component:About}
-]
--- taskboard-pro/src/app/features/tasks/tasks-page/routes.ts
+++
@@ -1,6 +0,0 @@
-import {Routes} from '@angular/router'
-import { TasksPage } from './tasks-page'
-
-export const TASK_ROUTES : Routes = [
-    {path: '', component:TasksPage}
-]
